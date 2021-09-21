b219c91adca2017d8b4c5e4c6a2c9f445d207bb9
Ryan <ryandefost@hotmail.com>
2021-09-21 15:05:10 +0200

    First (but late) commit

---
+++ Assets/Scripts/Bullet_Con.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Con : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int speed = 20;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        //rb.velocity = new Vector3(EnemyPosX, EnemyPosY, EnemyPosZ);
+         
+    }
+
+}
---
+++ Assets/Scripts/TowerShoot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    public GameObject bulletObject;
+    public GameObject BulletSpawner;
+
+    public float Range = 5f;
+
+    public float fireRate = 0.5f;
+    private float NextFire = 0.0f;
+
+    List <GameObject> EnterView = new List <GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            shoot();
+        }
+
+
+        // if the Target is "dead" it will not try and look for it
+        if (EnterView.Count > 0 && EnterView[0] != null)
+            {
+                // The tower will look at the first Target in the array
+                transform.LookAt(EnterView[0].transform, Vector3.up);
+                
+                if (Time.time > NextFire)
+                {
+                    NextFire = Time.time + fireRate;
+                    shoot();
+                }
+            }
+
+        if (EnterView.Count > 0 && EnterView[0] == null)
+            {
+                EnterView.Remove(EnterView[0]);
+            }
+
+    }
+
+    public void shoot()
+    {
+        GameObject bullet = Instantiate(bulletObject, BulletSpawner.transform.position , transform.rotation);
+        Destroy(bullet, Range);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "target")
+        {
+            EnterView.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "target")
+        {
+            //When it leaves the range it will look at the next one
+            EnterView.Remove(other.gameObject);
+        }
+
+    }
+
+
+}
